3903b746f2e79de16b4f53d98ae1db3611107b22
Dmdv <dimos-d@yandex.ru>
2013-05-20 22:33:54 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var MovieTracker = Ember.Application.create();
+
+MovieTracker.Router.map(function() {
+  // put your routes here
+});
+
+MovieTracker.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
---
+++ js/models/movie.js
@@ -0,0 +1,6 @@
+MovieTracker.Movie = Ember.Object.extend({
+	id: null,
+	title: null,
+	watched: false,
+	rating: 0
+});
